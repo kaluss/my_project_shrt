d76a7268429e1bf9eba9c17e77756ccb1b4ef3c0
thomas <tomasz_kaleta@mathart.pl>
2012-07-11 18:19:45 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyProjectShrt::Application.config.secret_token = '4ee1af275df3ac34b48b6bd36b8f02e3ac42ae20a0f65c047a03698cca9673ffe2d7e7f3fe35d5ea7bb583f6d40d9ad138b9a82217c7d62b5952be5131298208'
